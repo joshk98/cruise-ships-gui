d6c77a1828c17fe01ec59f0b1cd3e23714478fc0
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-04-02 16:07:57 +0100

    Test

--- src/Itinerary.js
+++ src/Itinerary.js
@@ -1,11 +1,13 @@
-class Itinerary {
-  constructor(ports) {
-    this.ports = ports;
+(function exportItinerary() {
+  class Itinerary {
+    constructor(ports = []) {
+      this.ports = ports;
+    }
   }
-}
 
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = Itinerary;
-} else {
-  window.Port = Itinerary;
-}
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Itinerary;
+  } else {
+    window.Itinerary = Itinerary;
+  }
+}());
--- src/Port.js
+++ src/Port.js
@@ -1,25 +1,25 @@
-class Port {
-  constructor(name) {
-    this.name = name;
-    this.ships = [];
+(function exportPort() {
+  class Port {
+    constructor(name) {
+      this.name = name;
+      this.ships = [];
+    }
+
+    addShip(ship) {
+      const ships = this.ships;
+      ships.push(ship);
+    }
+
+    removeShip(ship) {
+      const ships = this.ships;
+      const shipIndex = ships.indexOf(ship);
+      ships.splice(shipIndex, 1);
+    }
   }
 
-  addShip(ship) {
-    const ships = this.ships;
-
-    ships.push(ship);
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Port;
+  } else {
+    window.Port = Port;
   }
-
-  removeShip(ship) {
-    const ships = this.ships;
-    const shipIndex = ships.indexOf(ship);
-
-    ships.splice(shipIndex, 1);
-  }
-}
-
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = Port;
-} else {
-  window.Port = Port;
-};
+}());
